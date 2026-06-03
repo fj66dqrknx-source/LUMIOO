fa9edd055c5008972e9d0711f8a781d8bfdfaa7c
Ryan <belloryan82@gmail.com>
2026-06-03 15:39:07 +0200

    Commit message: Update profile-product branch with latest changes from main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/clerk-react";
 import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import ProfilePage from "./pages/ProfilePage";
@@ -10,10 +10,15 @@ import useAuthReq from "./hooks/useAuthReq";
 import useUserSync from "./hooks/useUserSync";
 
 function App() {
+  const { isClerkLoaded, isSignedIn } = useAuthReq();
+  useUserSync();
+
+  if (!isClerkLoaded) return null;
+
   return (
-    <div className="App">
-      <main>
-        <Navbar />
+    <div className="min-h-screen bg-base-100">
+      <Navbar />
+      <main className="max-w-5xl mx-auto px-4 py-8">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/:id" element={<ProductPage />} />
